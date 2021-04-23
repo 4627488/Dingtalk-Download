a8d07f6e2e4fabc7fb0b40e275d7c26caa8ddd45
4627488 <huangyunqings@outlook.com>
2021-04-23 14:10:20 +0800

    upd to void 403

--- downm3u8.py
+++ downm3u8.py
@@ -1,5 +1,5 @@
-#codeing=utf-8
-from requests import session, packages
+# codeing=utf-8
+import requests
 from os import makedirs, remove
 from re import sub
 from time import time
@@ -7,59 +7,68 @@ import os
 import re
 import sys
 
-def dl_aria2(url,dir,name):
-    os.system(f"aria2c -o {name} --dir={dir} \"{url}\"");
+ua = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36 dingtalk-win/1.0.0 nw(0.14.7) DingTalk(6.0.12-Release.4190287) Mojo/1.0.0 Native AppType(release) Channel/201200'}
 
-packages.urllib3.disable_warnings()
-x = session()
+
+def dl_aria2(url, dir, name):
+    os.system(f"aria2c -o {name} --dir={dir} \"{url}\"")
+
+
+def dl_request(url, dir, name):
+    r = requests.get(url, headers=ua)
+    with open(dir+name, "wb") as code:
+        code.write(r.content)
+
+
+requests.packages.urllib3.disable_warnings()
+x = requests.session()
 
 tmp = input('URL: ')
 
 pattern2 = re.compile('\w{8}(-\w{4}){3}-\w{12}')
 pattern1 = re.compile('\S*://\S+.alicdn.com/.*?/')
 
-name = re.search(pattern2,tmp).group(0)
-host = re.search(pattern1,tmp).group(0)
-path = 'download/temp/'+ name +"/"
-head = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 dingtalk-win/1.0.0 nw(0.14.7) DingTalk(6.0.8-Release.30803) Mojo/1.0.0 Native AppType(release) Channel/201200'}
+name = re.search(pattern2, tmp).group(0)
+host = re.search(pattern1, tmp).group(0)
+path = 'download/temp/' + name + "/"
 n = 0
-total=0
-try: 
+total = 0
+try:
     makedirs(path)
-except: 
+except:
     pass
 
-dl_aria2(tmp,path,name+".m3u8")
-filem3u8=open(path+name+'.m3u8')
+dl_request(tmp, path, name+".m3u8")
+filem3u8 = open(path+name+'.m3u8')
 
 while True:
-    tp2=filem3u8.readline()
-    if tp2=='':
+    tp2 = filem3u8.readline()
+    if tp2 == '':
         break
     if tp2.startswith('#'):
         continue
-    total+=1
+    total += 1
 
 filem3u8.seek(0)
 
 # 下载
 while True:
     try:
-        tp2=filem3u8.readline()
-        if tp2=='':
+        tp2 = filem3u8.readline()
+        if tp2 == '':
             print('Finished!')
             break
         if tp2.startswith('#'):
             continue
-        #print(tp2)
-        tp2=tp2.strip();
-        print(f'Downloading [{n+1}/{total}] ...',end='')
+        # print(tp2)
+        tp2 = tp2.strip()
+        print(f'Downloading [{n+1}/{total}] ...', end='')
         sys.stdout.flush()
-        dl_aria2(host+tp2,path,f"{n+1}.ts")
+        dl_request(host+tp2, path, f"{n+1}.ts")
     except Exception as e:
         print(f'Fail to download {n+1}.ts \n {e}')
         break
-    n+=1
+    n += 1
     print('[done]')
 
 # 整合
@@ -71,9 +80,9 @@ with open(p, 'wb') as f:
             f.write(tmp.read())
         remove(f'{path}{i+1}.ts')
 
-#删除无用文件
+# 删除无用文件
 filem3u8.close()
 f.close()
-remove(f'{path}{name}.m3u8')
+# remove(f'{path}{name}.m3u8')
 
 a = input('Done. Press enter to exit.')
